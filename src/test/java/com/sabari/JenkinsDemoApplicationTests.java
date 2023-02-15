f580e0a397b88e5bf59022b3691a5c49a2e19a50
Sabarigirinathan <sabari_mb80@yahoo.co.in>
2023-02-15 07:04:01 +0000

    Initial Source code commit

---
+++ src/test/java/com/sabari/JenkinsDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sabari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+		@Test
+			void contextLoads() {
+					}
+
+}
+
